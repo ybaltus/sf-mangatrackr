4f000ecf18c7c07e9b1e226981ca8fa35284e8ea
Yannis <yannis.baltus@email.com>
2023-11-25 10:17:44 +0100

    ✨ Init the admin dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Editor;
+use App\Entity\Fantrad;
+use App\Entity\Manga;
+use App\Entity\MangaStatus;
+use App\Entity\MangaType;
+use App\Entity\MangaUserTrack;
+use App\Entity\StatusTrack;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect(
+            $adminUrlGenerator->setController(MangaCrudController::class)->setAction(Action::INDEX)->generateUrl()
+        );
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sf Mangatrackr');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::LinkToCrud('Editor', 'fa fa-user-pen', Editor::class);
+        yield MenuItem::LinkToCrud('Fantrad', 'fa-brands fa-jedi-order', Fantrad::class);
+        yield MenuItem::LinkToCrud('Manga', 'fa fa-book', Manga::class);
+        yield MenuItem::LinkToCrud('MangaStatus', 'fa fa-info-circle', MangaStatus::class);
+        yield MenuItem::LinkToCrud('MangaType', 'fa fa-info-circle', MangaType::class);
+        yield MenuItem::LinkToCrud('StatusTrack', 'fa fa-info-circle', StatusTrack::class);
+        yield MenuItem::LinkToCrud('User', 'fa fa-user', User::class);
+        yield MenuItem::LinkToCrud('UserTrackList', 'fa-solid fa-list-check', MangaUserTrack::class);
+    }
+}
---
+++ src/Controller/Admin/MangaStatusCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\MangaStatus;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class MangaStatusCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return MangaStatus::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
