225b2ca5fb6bfc233a041843ad6e1baa45fc25c5
Yannis <yannis.baltus@email.com>
2023-12-04 20:22:58 +0100

    📦️ Add Sf-ux-autocomplete

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
