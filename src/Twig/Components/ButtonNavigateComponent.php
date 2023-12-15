f27a18e388449f2993f84f6502074052ec78057e
Yannis <yannis.baltus@email.com>
2023-12-15 17:54:37 +0100

    Some ui modification

--- src/Twig/Components/ButtonNavigateComponent.php
+++ src/Twig/Components/ButtonNavigateComponent.php
@@ -5,8 +5,10 @@ namespace App\Twig\Components;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
-final class ButtonReturnComponent
+final class ButtonNavigateComponent
 {
     public string $title;
-    public string $pathReturn;
+    public string $routeName;
+
+    public string $direction = 'left';
 }
