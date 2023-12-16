9b00b78ffa2bc38cc3ba99fd047a196638ae2ac7
Yannis <yannis.baltus@email.com>
2023-12-16 16:32:36 +0100

    🚧 Add step for scantheque and some fix

---
+++ src/Twig/Components/StepInfosComponent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class StepInfosComponent
+{
+    /**
+     * @var array<string>
+     */
+    public array $titles;
+
+    /**
+     * @var array<string>
+     */
+    public array $contents;
+}
