9f94fbebb936a77997f7a10b557235a7cb6ad8cc
Yannis <yannis.baltus@email.com>
2023-12-20 21:54:16 +0100

    ✨ Redirect to scantheque after login

--- src/Security/Auth/AppAuthenticator.php
+++ src/Security/Auth/AppAuthenticator.php
@@ -48,7 +48,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home_index'));
+        return new RedirectResponse($this->urlGenerator->generate('scantheque_index'));
     }
 
     protected function getLoginUrl(Request $request): string
