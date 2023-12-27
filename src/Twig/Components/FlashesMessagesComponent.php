1f7993047bbe4357b6c760703e21fb2db64afb21
Yannis <yannis.baltus@email.com>
2023-12-27 15:03:20 +0100

    ✨ Add a FlashesMessagesComponent

---
+++ src/Twig/Components/FlashesMessagesComponent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class FlashesMessagesComponent
+{
+    /**
+     * @var array<string>
+     */
+    public array $flashesMessageTarget;
+
+    public string $flashMessageType = 'simple';
+
+    /**
+     * @var array<string>
+     */
+    public array $listTypeFlashMessage = ['info', 'warning', 'danger', 'success', 'simple'];
+}
