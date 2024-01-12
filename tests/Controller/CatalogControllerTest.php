2ef2397307044ec7977e306efc45f61abb15fb2c
Yannis <yannis.baltus@email.com>
2024-01-12 13:32:45 +0100

    test: Edit CatalogControllerTest

--- tests/Controller/CatalogControllerTest.php
+++ tests/Controller/CatalogControllerTest.php
@@ -56,4 +56,35 @@ class CatalogControllerTest extends WebTestCase implements ControllerTestInterfa
         $crawler = $client->request('GET', '/catalog');
         $this->assertSelectorExists('input[name=adult-input]');
     }
+
+    public function testInputWithHTMXAttributes(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/catalog');
+        $this->assertSelectorExists('form[hx-post="/manga/search"]');
+        $this->assertSelectorExists('form[hx-trigger="submit delay:500ms"]');
+        $this->assertSelectorExists('form[hx-target="#search-results"]');
+        $this->assertSelectorExists('form[hx-indicator="#search-indicator"]');
+    }
+
+    public function testSearchCatalogStimulusController(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/catalog');
+        $this->assertSelectorExists('form[data-controller=search-catalog]');
+    }
+
+    public function testSubmitSearchyOnSubmitEventStimulusAction(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/catalog');
+        $this->assertSelectorExists('form[data-action="submit->search-catalog#submitSearch"]');
+    }
+
+    public function testHiddenGalleryOnKeyUpEventStimulusAction(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/catalog');
+        $this->assertSelectorExists('input[data-action="keyup->search-catalog#hiddenGallery"]');
+    }
 }
