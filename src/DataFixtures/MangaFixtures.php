7136d5fabed256859b5dc75329c9fb5251b7b2fa
Yannis <yannis.baltus@email.com>
2024-01-13 13:43:31 +0100

    page: Add Terms Of Use

--- src/DataFixtures/MangaFixtures.php
+++ src/DataFixtures/MangaFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Entity\Manga;
 use App\Entity\MangaJikanAPI;
-use App\Entity\MangaStatistic;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
