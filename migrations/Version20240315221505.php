0f0cd38d54eb9adfd4db61af7edbe8431068d4d1
Yannis <yannis.baltus@email.com>
2024-03-15 23:27:23 +0100

    oauthGoogle: Update User entity

---
+++ migrations/Version20240315221505.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240315221505 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '[OAuth Google] Update User entity to store Google ID and last connected date';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD id_google VARCHAR(255) DEFAULT NULL, ADD last_connected_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP id_google, DROP last_connected_at');
+    }
+}
