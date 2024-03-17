224d0c129768c29562c1bc0152561a241d19ea6f
Yannis <yannis.baltus@email.com>
2024-03-17 14:15:06 +0100

    oauthGoogle: Auth limit for only for some test emails

--- src/Security/OAuth/GoogleAuthenticator.php
+++ src/Security/OAuth/GoogleAuthenticator.php
@@ -4,24 +4,40 @@ namespace App\Security\OAuth;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Provider\GoogleUser;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 final class GoogleAuthenticator extends AbstractOAuthAuthenticator
 {
     protected string $providerName = 'google';
+
+    public function __construct(
+        ClientRegistry $registry,
+        RouterInterface $router,
+        UserRepository $userRepository,
+        private string $oauthTestEmails
+    ) {
+        parent::__construct($registry, $router, $userRepository);
+    }
 
     protected function getUserFromRessourceOwner(
         ResourceOwnerInterface $resourceOwner,
         UserRepository $userRepository
     ): ?User {
         if (!($resourceOwner instanceof GoogleUser)) {
-            throw new \LogicException('Resource owner must be an instance of GoogleUser');
+            throw new \RuntimeException('Resource owner must be an instance of GoogleUser');
+        }
+
+        // Check if email is in test emails (**** Remove later ****)
+        if (!in_array($resourceOwner->getEmail(), explode(',', $this->oauthTestEmails))) {
+            throw new \RuntimeException('You are not allowed to use this application');
         }
 
         // Check if email is already verified
         if (false === ($resourceOwner->toArray()['email_verified'] ?? false)) {
-            throw new \LogicException('Email must be verified');
+            throw new \RuntimeException('Email must be verified');
         }
 
         $user = $userRepository->findOneBy([
