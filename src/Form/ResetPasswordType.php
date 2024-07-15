85b531f63df591ee548119c9230358b9c78d8608
Yannis <yannis.baltus@email.com>
2024-07-15 19:34:21 +0200

    translations: Sign in page

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -18,37 +19,34 @@ class ResetPasswordType extends AbstractType
         $builder
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
+                'invalid_message' => new TranslatableMessage('user.form.invalid_password', [], 'app'),
                 'required' => true,
                 'first_options' => [
-                    'label' => 'Mot de passe',
+                    'label' => new TranslatableMessage('user.form.password', [], 'app'),
                     'attr' => [
                         'minlength' => 8,
                         'maxlength' => 4096,
                     ],
                     'toggle' => true,
-                    'hidden_label' => 'Masquer',
-                    'visible_label' => 'Afficher',
+                    'hidden_label' => new TranslatableMessage('user.form.hide', [], 'app'),
+                    'visible_label' => new TranslatableMessage('user.form.show', [], 'app'),
                     'button_classes' => ['toggle-password-button', 'text-app-clear-black', 'dark:text-app-dark-gray2'],
                 ],
                 'second_options' => [
-                    'label' => 'Confirmez le mot de passe',
+                    'label' => new TranslatableMessage('user.form.new_password_confirm', [], 'app'),
                     'attr' => [
                         'minlength' => 8,
                         'maxlength' => 4096,
                     ],
                     'toggle' => true,
-                    'hidden_label' => 'Masquer',
-                    'visible_label' => 'Afficher',
+                    'hidden_label' => new TranslatableMessage('user.form.hide', [], 'app'),
+                    'visible_label' => new TranslatableMessage('user.form.show', [], 'app'),
                     'button_classes' => ['toggle-password-button', 'text-app-clear-black', 'dark:text-app-dark-gray2'],
                 ],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez saisir un mot de passe',
-                    ]),
+                    new NotBlank(),
                     new Length([
                         'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
